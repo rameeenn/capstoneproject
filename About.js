06e6ca5118a5309545936db670ca2434d9778589
rameeenn <119961810+rameeenn@users.noreply.github.com>
2023-08-12 15:27:32 +0500

    Add files via upload

--- About.js
+++ About.js
@@ -6,6 +6,12 @@ const About = () =>{
             <h2 style={{
                 textAlign: 'center', justifyContent: 'center',
             }}>Little Lemon Restaurant</h2>
+            <p style={{
+                textAlign: 'center', justifyContent: 'center',
+            }}>Delivering quality dishes to you!</p>
+            <p style={{
+                textAlign: 'center', justifyContent: 'center',
+            }}>Book a table, or order at your doorstep!</p>
         </div>
     )
 }
--- Menu.js
+++ Menu.js
@@ -6,6 +6,9 @@ const About = () =>{
             <h2 style={{
                 textAlign: 'center', justifyContent: 'center',
             }}>Menu</h2>
+            <p> style={{
+                textAlign: 'center', justifyContent: 'center',
+            }}Discover our mouth-watering dishes! </p>
         </div>
     )
 }
